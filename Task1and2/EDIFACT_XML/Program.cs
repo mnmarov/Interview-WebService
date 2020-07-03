b8c47142294a8bc14246a6901fc7b3933e3c75ca
Marko Marov <mnmarov@yahoo.com>
2020-07-03 16:00:15 +0100

    Add files via upload

---
+++ Task1and2/EDIFACT_XML/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace EDIFACT_XML
+{
+    public class Location
+    {
+        public int Number;
+        public string Text; 
+    }
+
+    public class Reference
+    {
+        public string Code;
+        public string Value;
+    }
+
+    public class Program
+    {
+
+        public static List<Location> ParseEDIFACT(Stream stream)
+        {
+            var result = new List<Location>();
+            if (stream == null)
+                return result;
+            StreamReader sr = new StreamReader(stream);
+            while (sr.Peek() >= 0)
+            {
+                var line = sr.ReadLine();
+                if (line.Trim().ToLower().StartsWith("loc"))
+                {
+                    var split = line.Split(new char[] { '+' }, StringSplitOptions.RemoveEmptyEntries);
+                    if (split.Length < 3)
+                        throw new Exception("Incorrect format near: '" + line + "'");
+                    int num = 0;
+                    if (!int.TryParse(split[1], out num))
+                        throw new Exception("Incorrect number format: '" + split[1] + "'");
+                    // remove the segment terminator from the text
+                    var text = split[2].TrimEnd(new char[] { '\'' });
+                    result.Add(new Location() { Number = num, Text = text });
+                }
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Note: we would be deserializing the whle XML to an object in a real application
+        /// </summary>
+        /// <param name="stream"></param>
+        /// <param name="codes"></param>
+        /// <returns></returns>
+        public static List<Reference> ParseReferences(Stream stream, string[] lookup_codes)
+        {
+            var result = new List<Reference>();
+            if (stream == null || lookup_codes == null || lookup_codes.Length == 0)
+                return result;
+            XmlDocument doc = new XmlDocument();
+            doc.Load(stream);
+            var nodes = doc.SelectNodes("//Declaration/DeclarationHeader/Reference");
+            foreach(XmlElement node in nodes)
+            {
+                var code = node.GetAttribute("RefCode").Trim();
+                if (lookup_codes.Contains(code))
+                {
+                    result.Add(new Reference() { Code = code, Value = node.InnerText });
+                }
+            }
+            return result;
+        }
+
+        static void Main(string[] args)
+        {
+            string FileName = "EDIFACT.txt";
+
+            if (!File.Exists(FileName))
+                throw new Exception(FileName + " not found.");
+
+            // Task 1.
+            Console.WriteLine("Task 1.");
+            var Locations = ParseEDIFACT(File.OpenRead(FileName));
+            foreach(var loc in Locations)
+            {
+                Console.WriteLine("Location: " + loc.Number + " " + loc.Text);
+            }
+
+            FileName = "Tracking.xml";
+            if (!File.Exists(FileName))
+                throw new Exception(FileName + " not found.");
+
+            // Task 2.
+            Console.WriteLine("Task 2.");
+            var References = ParseReferences(File.OpenRead(FileName), new string[] { "MWB", "TRV", "CAR"});
+            foreach (var Ref in References)
+            {
+                Console.WriteLine("Reference code: " + Ref.Code + " value: " + Ref.Value);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Task1and2/UnitTests/UnitTest1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EDIFACT_XML;
+using System.IO;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        public static Stream ToStream(string str)
+        {
+            MemoryStream stream = new MemoryStream();
+            StreamWriter writer = new StreamWriter(stream);
+            writer.Write(str);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+
+        [TestMethod]
+        public void TestEDIFAX()
+        {
+            string correct_format = "LOC+18+SOL'\r\nLOC+35+SE'\r\nLOC+36+TZ'";
+
+            var test = Program.ParseEDIFACT(ToStream(correct_format));
+            if (test.Count == 3)
+            {
+                Assert.AreEqual(test[1].Number, 35);
+            }
+            else
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void TestEDIFAXFail()
+        {
+            string wrong_format = "LOC+kkkk+SOL";
+            try
+            {
+                var test = Program.ParseEDIFACT(ToStream(wrong_format));
+            } catch(Exception ex)
+            {
+                if (!ex.Message.StartsWith("Incorrect number format:"))
+                    Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void TestMalformedXML()
+        {
+            string MissingClosingTag = "<InputDocument>" +
+"  <DeclarationList>" +
+"    <Declaration Command=\"DEFAULT\" Version=\"5.13\">" +
+"      <DeclarationHeader>" +
+"        <Jurisdiction>IE</Jurisdiction>" +
+"        <CWProcedure>IMPORT</CWProcedure>" +
+"        <DeclarationDestination>CUSTOMSWAREIE</DeclarationDestination>" +
+"        <DocumentRef>71Q0019681</DocumentRef>" +
+"        <SiteID>DUB</SiteID>" +
+"        <AccountCode>G0779837</AccountCode>" +
+"        <Reference RefCode=\"MWB\">" +
+"          <RefText>586133622</RefText>" +
+"        </Reference>" +
+"        <Reference RefCode=\"KEY\">" +
+"          <RefText>DUB16049</RefText>" +
+"        </Reference>" +
+"        <Reference RefCode=\"CAR\">" +
+"          <RefText>71Q0019681</RefText>" +
+"        </Reference>" +
+"        <Reference RefCode=\"COM\">" +
+"          <RefText>71Q0019681</RefText>" +
+"        </Reference>" +
+"        <Reference RefCode=\"SRC\">" +
+"          <RefText>ECUS</RefText>" +
+"        </Reference>" +
+"        <Reference RefCode=\"TRV\">" +
+"          <RefText>1</RefText>" +
+"        </Reference>" +
+"        <Reference RefCode=\"CAS\">" +
+"          <RefText>586133622</RefText>" +
+"        </Reference>" +
+"        <Reference RefCode=\"HWB\">" +
+"          <RefText>586133622</RefText>" +
+"        </Reference>" +
+"        <Reference RefCode=\"UCR\">" +
+"          <RefText>586133622</RefText>" +
+"        </Reference>" +
+"        <Country CodeType=\"NUM\" CountryType=\"Destination\">IE</Country>" +
+"        <Country CodeType=\"NUM\" CountryType=\"Dispatch\">CN</Country>" +
+" " +
+"          </DeclarationHeader>" +
+"</DeclarationList>" +
+"</InputDocument>";
+
+            try
+            {
+                var test = Program.ParseReferences(ToStream(MissingClosingTag), new string[] { "MWB", "TRV", "CAR" });
+                Assert.Fail();
+            }
+            catch (System.Xml.XmlException ex)
+            {
+                if (!ex.Message.StartsWith("The 'Declaration' start tag on"))
+                    Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void TestXML()
+        {
+            string xml1 = "<DeclarationList><Declaration><DeclarationHeader><Reference RefCode=\"1\"><RefText>586133622</RefText></Reference></DeclarationHeader></Declaration></DeclarationList>";
+            var test = Program.ParseReferences(ToStream(xml1), new string[] { "1", "2", "3" });
+            Assert.AreEqual(test.Count, 1);
+            test = Program.ParseReferences(ToStream(xml1), new string[] { "3" });
+            Assert.AreEqual(test.Count, 0);
+        }
+
+    }
+}
